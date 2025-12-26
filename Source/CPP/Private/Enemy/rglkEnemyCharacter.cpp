3f896036e371ca50910ddc22e87742a18cde80be
torasandhi <torasandhipakey@gmail.com>
2025-12-26 22:41:28 +0800

    enum state machine (refactor tmrw)

--- Source/CPP/Private/Enemy/rglkEnemyCharacter.cpp
+++ Source/CPP/Private/Enemy/rglkEnemyCharacter.cpp
@@ -24,27 +24,13 @@ void ArglkEnemyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	FindTarget();
+	SetState(EEnemyState::Chasing);
 }
 
 void ArglkEnemyCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	if (TargetActor)
-	{
-		ChaseTarget();
-	}
-	else
-	{
-		FindTarget();
-	}
-
-	if (!SeparationForce.IsZero())
-	{
-		AddMovementInput(SeparationForce, 1.0f);
-
-		SeparationForce = FMath::VInterpTo(SeparationForce, FVector::ZeroVector, DeltaTime, 5.0f);
-	}
+	UpdateState(DeltaTime);
 }
 
 void ArglkEnemyCharacter::FindTarget()
@@ -55,24 +41,28 @@ void ArglkEnemyCharacter::FindTarget()
 void ArglkEnemyCharacter::ChaseTarget()
 {
 	if (!TargetActor) return;
-	
-	float Distance = FVector::Dist(GetActorLocation(), TargetActor->GetActorLocation());
 
-	// If outside stopping distance, Move
+	float Distance = FVector::Dist(GetActorLocation(), TargetActor->GetActorLocation());
 	if (Distance > StopDistance)
 	{
 		FVector Direction = (TargetActor->GetActorLocation() - GetActorLocation()).GetSafeNormal();
-
-		// 2. Move!
-		// AddMovementInput uses the CharacterMovementComponent (handles gravity, slopes, etc).
 		AddMovementInput(Direction, 1.0f);
 	}
 	else
 	{
-		if (!WeaponComp) return;
-		Attack();
-	} 
+	}
 }
+
+bool ArglkEnemyCharacter::TimerManager(const FTimerHandle MyTimerHandle) const
+{
+	FTimerManager& TimerManager = GetWorldTimerManager();
+
+	if (TimerManager.IsTimerActive(MyTimerHandle))
+		return true;
+	else
+		return false;
+}
+
 
 void ArglkEnemyCharacter::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp,
                                     bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse,
@@ -109,9 +99,8 @@ void ArglkEnemyCharacter::Die()
 void ArglkEnemyCharacter::Attack()
 {
 	PRINT_DEBUG_MESSAGE("ENEMY IS ATTACKING");
+	AttackTimer.Invalidate();
 }
-
-// --- POOLING LOGIC --- //
 
 void ArglkEnemyCharacter::OnSpawnFromPool_Implementation()
 {
@@ -124,4 +113,99 @@ void ArglkEnemyCharacter::OnSpawnFromPool_Implementation()
 	// Ensure Physics is on
 	SetActorHiddenInGame(false);
 	SetActorEnableCollision(true);
+}
+
+
+// Refactor to 1 class per state
+// -- Handle States --
+
+void ArglkEnemyCharacter::SetState(EEnemyState NewState)
+{
+	if (CurrentState == NewState) return;
+
+	ExitState(CurrentState);
+	CurrentState = NewState;
+	EnterState(CurrentState);
+}
+
+void ArglkEnemyCharacter::EnterState(EEnemyState State)
+{
+	switch (State)
+	{
+	case EEnemyState::Chasing:
+
+		break;
+
+	case EEnemyState::Attacking:
+		if (!WeaponComp)
+			SetState(EEnemyState::Chasing);
+		break;
+	}
+}
+
+void ArglkEnemyCharacter::ExitState(EEnemyState State)
+{
+	switch (State)
+	{
+	case EEnemyState::Attacking:
+
+		break;
+
+	default:
+		break;
+	}
+}
+
+void ArglkEnemyCharacter::UpdateState(float DeltaTime)
+{
+	switch (CurrentState)
+	{
+	case EEnemyState::Chasing:
+		UpdateChase(DeltaTime);
+		break;
+
+	case EEnemyState::Attacking:
+		UpdateAttack(DeltaTime);
+		break;
+	}
+}
+
+void ArglkEnemyCharacter::UpdateChase(float DeltaTime)
+{
+	float Distance = FVector::Dist(GetActorLocation(), TargetActor->GetActorLocation());
+
+	if (TargetActor)
+		ChaseTarget();
+	else
+		FindTarget();
+
+	// basic seperation logic
+	if (!SeparationForce.IsZero())
+	{
+		AddMovementInput(SeparationForce, 1.0f);
+		SeparationForce = FMath::VInterpTo(
+			SeparationForce,
+			FVector::ZeroVector,
+			DeltaTime,
+			5.0f);
+	}
+
+	if (Distance < StopDistance)
+		SetState(EEnemyState::Attacking);
+}
+
+void ArglkEnemyCharacter::UpdateAttack(float DeltaTime)
+{
+	if (TimerManager(AttackTimer)) return;
+	GetWorldTimerManager().SetTimer(
+		AttackTimer,
+		this,
+		&ArglkEnemyCharacter::Attack,
+		2.f,
+		false
+	);
+
+	float Distance = FVector::Dist(GetActorLocation(), TargetActor->GetActorLocation());
+	if (Distance > StopDistance)
+		SetState(EEnemyState::Chasing);
 }
--- Source/CPP/Public/Enemy/rglkEnemyCharacter.h
+++ Source/CPP/Public/Enemy/rglkEnemyCharacter.h
@@ -1,13 +1,22 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "rglkCharacter.h" 
+#include "rglkCharacter.h"
 #include "UPoolableInterface.h"
 #include "rglkEnemyCharacter.generated.h"
+
+UENUM(BlueprintType)
+enum class EEnemyState : uint8
+{
+	Chasing				UMETA(DisplayName="Chasing"),
+	Attacking			UMETA(DisplayName="Attacking")
+};
+
 
 UCLASS()
 class CPP_API ArglkEnemyCharacter : public ArglkCharacter, public IPoolableInterface
 {
+	
 	GENERATED_BODY()
 
 public:
@@ -16,29 +25,42 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
-    
+
 	// Override NotifyHit to keep your "Bumping" logic
-	virtual void NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+	virtual void NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, class UPrimitiveComponent* OtherComp,
+	                       bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse,
+	                       const FHitResult& Hit) override;
 	virtual void Die() override;
 	virtual void Attack() override;
-	
+
 	// Interface Implementation
 	virtual void OnSpawnFromPool_Implementation() override;
 
 private:
-	// Logic to find player
 	void FindTarget();
-    
-	// Logic to chase
 	void ChaseTarget();
+	bool TimerManager(const FTimerHandle MyTimerHandle) const;
 
-private:
 	UPROPERTY(VisibleAnywhere, Category = "AI")
 	AActor* TargetActor;
-
 	UPROPERTY(EditAnywhere, Category = "AI")
-	float StopDistance = 100.0f; // Increased slightly for stability
-
-	// Used for the separation logic
+	float StopDistance = 100.0f;
+	
 	FVector SeparationForce;
+	FTimerHandle AttackTimer;
+
+
+	// handle current state
+	
+	UPROPERTY(VisibleAnywhere, Category = "AI")
+	EEnemyState CurrentState = EEnemyState::Chasing;
+	
+	void SetState(EEnemyState NewState);
+
+	void EnterState(EEnemyState State);
+	void ExitState(EEnemyState State);
+	void UpdateState(float DeltaTime);
+
+	void UpdateChase(float DeltaTime);
+	void UpdateAttack(float DeltaTime);
 };
